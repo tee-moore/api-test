84a6cee4e152f3df73eefb49de12f1957652acf9
tee-moore <2teemoore@gmail.com>
2019-11-02 14:24:29 +0200

    Added preorders table migration

---
+++ database/migrations/2019_11_01_114057_create_preorders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePreordersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('preorders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email');
+            $table->bigInteger('movie_id')->unsigned();
+            $table->foreign('movie_id')
+                ->references('id')
+                ->on('movies')
+                ->onDelete('RESTRICT')
+                ->onUpdate('CASCADE');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('preorders');
+    }
+}
