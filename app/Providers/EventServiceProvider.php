21f0efa5b0d0c35191edca55866059b42cb267c4
tee-moore <2teemoore@gmail.com>
2019-11-02 14:34:19 +0200

    Added event and listeners to EventServiceProvider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Events\PreorderCreated;
+use App\Listeners\SendEmailPreorderNotification;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -17,6 +19,9 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+        PreorderCreated::class => [
+            SendEmailPreorderNotification::class,
         ],
     ];
 
